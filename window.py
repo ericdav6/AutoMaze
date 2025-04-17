7a5898077cdc2a6936789b43748812734ddf3a1d
ericdav6 <195047077+ericdav6@users.noreply.github.com>
2025-04-16 17:24:28 -0700

    add tkinter window class

---
+++ window.py
@@ -0,0 +1,34 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window():
+    def __init__(self, width, height):                 
+        self.width = width
+        self.height = height
+
+        self.root = Tk()
+        self.root.title("Window")
+
+        self.canvas = Canvas(self.root, height=self.height, width=self.width)
+        self.canvas.pack()
+
+        self.window_running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close())
+
+    def redraw(self):
+        self.root.update()
+        self.root.update_idletasks()
+
+    def wait_for_close(self):
+        self.window_running = True
+
+        while self.window_running:
+            self.redraw()
+
+    def close(self):
+        self.window_running = False
+
+
+
+
+
+
